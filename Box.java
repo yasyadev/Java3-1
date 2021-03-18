75ab0381a7bcc178de87499db1c6ced9211b2baf
yasyadev <78085638+yasyadev@users.noreply.github.com>
2021-03-18 22:00:40 +0300

    Create Box.java

---
+++ Box.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Box<T extends Fruit> {
+    private ArrayList<T> box = new ArrayList<>();
+
+    public Box(){
+
+    }
+    public float getWeight(){
+        float weight = 0.0f;
+        for(T o : box){
+            weight += o.getWeight();
+        }return weight;
+    }
+    public boolean compare(Box anotherBox) {
+        if(getWeight() == anotherBox.getWeight()) return true;
+        return false;
+    }
+    public void pourTo(Box <T>anotherBox){
+        anotherBox.box.addAll(box);
+        box.clear();
+    }
+    //еще способ добавить фрукты
+    public void addFruit(T fruit, int amount){
+        for(int i=0;i<amount;i++){
+            box.add(fruit);
+        }
+    }
+}
